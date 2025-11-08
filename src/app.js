c3155366ba214b88d0b0ec0c1e783ff728746b2f
FirstlyBoldly <ankhbhold@gmail.com>
2025-11-09 01:01:12 +0900

    Moved display container event listeners to appropriate location.

--- src/app.js
+++ src/app.js
@@ -1,28 +1,11 @@
 import "./global.css";
 import { Board } from "./components/board";
-import { canvasEvents } from "./components/freehand";
-import { predictState } from "./services/tfjs";
 import { DisplayContainer } from "./components/display-container";
 
 const app = document.getElementById("app");
 if (app) {
-    const [sign, indicator] = DisplayContainer(app);
+    DisplayContainer(app);
     Board(app);
-
-    window.addEventListener(canvasEvents.canvasUpdated, () => {
-        const canvas = document.getElementById("front-canvas");
-        const prediction = predictState(canvas).toUpperCase();
-        if (prediction) {
-            sign.write(prediction);
-            indicator.write("?");
-        }
-    });
-
-    window.addEventListener(canvasEvents.canvasCleared, () => {
-        sign.reset();
-        indicator.write("-");
-    });
-
 } else {
     console.error("No element to append onto.");
 }
