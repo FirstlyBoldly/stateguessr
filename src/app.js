d84eae5abf95b8150eac666cee09e6ea4a120fcc
FirstlyBoldly <ankhbhold@gmail.com>
2025-11-09 05:58:17 +0900

    Initial design of menu.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import "./global.css";
 import { Board } from "./components/board";
 import { DisplayContainer } from "./components/display-container";
+import { Menu } from "./components/menu";
 
 const app = document.getElementById("app");
 if (app) {
+    const menu = Menu();
+    app.append(menu);
+
     DisplayContainer(app);
     Board(app);
 } else {
