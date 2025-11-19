746ed3a6d86d3b305f9b4ef8acc32d2ed83f381c
FirstlyBoldly <ankhbhold@gmail.com>
2025-11-20 03:30:46 +0900

    Minor adjustments.

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ startMenu.closeButton.addEventListener("click", () => {
 
     // Variables of the game loop.
     const MAX_ROUNDS = 5;
-    const ROUND_DURATION_IN_SECONDS = 10;
+    const ROUND_DURATION_IN_SECONDS = 20;
     const INTERMISSION_DURATION_IN_SECONDS = 2;
     const ROUND_STARTING_NUMBER = 0;
 
@@ -115,6 +115,8 @@ startMenu.closeButton.addEventListener("click", () => {
         disableFreehandShortcuts();
         clearInterval(roundInterval);
         clearInterval(timer.interval);
+
+        board.style.pointerEvents = "none";
 
         sign.unlock();
         indicator.unlock();
